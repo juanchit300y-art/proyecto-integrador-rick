c03b3af5334cbad1a9988b0005f4a74242800348
juanchit300y-art <juanchit300y@gmail.com>
2026-09-24 09:23:43 -0500

    feat: config on html, functions on js and some implementations, just grid on css

--- script.js
+++ script.js
@@ -44,12 +44,69 @@ function filtrarPorEspecie(personajes, especie) {
   // dejando solo los que coinciden (repasa el ejercicio 3 de la práctica).
 }
 
+//Ejercicio 4
+function obtenerNombres(personajes) {
+  return personajes.map(function (personaje) {
+    return personaje.name;
+  });
+}
+
+//Ejercicio 5
+function buscarPorNombre(personajes, nombre) {
+  return personajes.find(function (personaje) {
+    return personaje.name === nombre;
+  });
+}
+
+//Ejercicio 6
+function hayPersonajesMuertos(personajes) {
+  return personajes.some(function (personaje) {
+    return personaje.status === "Dead";
+  });
+}
+
+//Ejercicio 7
+function todosVivos(personajes) {
+  return personajes.every(function (personaje) {
+    return personaje.status === "Alive";
+  });
+}
+
+//Ejercicio 8
+function ordenarPorNombre(personajes) {
+  return [...personajes].sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+}
+
+//Ejercicio 9
+function primeros(personajes, cantidad) {
+  return personajes.slice(0, cantidad);
+}
+
+//Ejercicio 10
+function posicionDeNombre(nombres, nombre) {
+  return nombres.indexOf(nombre);
+}
+
+//Ejercicio 11
+function contarVivos(personajes) {
+  return personajes.reduce(function (total, personaje) {
+    return personaje.status === "Alive" ? total + 1 : total;
+  }, 0);
+}
+
 let personajes = [];
+
+let ordenadoAZ = false;
 
 function aplicarFiltros() {
   const nombre = document.querySelector("#filtro-nombre").value.trim().toLowerCase();
   const estado = document.querySelector("#filtro-estado").value;
   const especie = document.querySelector("#filtro-especie").value;
+
+  //verifica si la casilla está marcada
+  const primeros10 = document.querySelector("#check-primeros").checked;
 
   let filtrados = filtrarPorEstado(personajes, estado);
   filtrados = filtrarPorEspecie(filtrados, especie);
@@ -57,7 +114,19 @@ function aplicarFiltros() {
     return personaje.name.toLowerCase().includes(nombre);
   });
 
+  //aplicamos la funcionalidad para ordenar
+  if (ordenadoAZ){
+    filtrados = ordenarPorNombre(filtrados);
+  }
+
+  //aplicamos la funcionalidad de mostrar solo los primeros 10
+  if (primeros10){
+    filtrados = primeros(filtrados, 10);
+  }
+
   pintarResultados(filtrados);
+  //funcion para los textos y el badge
+  actualizarEstadisticas(filtrados);
 }
 
 function pintarResultados(lista) {
