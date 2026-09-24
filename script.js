149a7192843efe323f6fa8d5e821f52fff530a06
juanchit300y-art <juanchit300y@gmail.com>
2026-09-24 13:51:41 -0500

    feat: made the filter clean function, modifications, on the actualizarEstadisticas function

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ function contarVivos(personajes) {
 
 let personajes = [];
 
+//inicializo el orden como desactivado
 let ordenadoAZ = false;
 
 function aplicarFiltros() {
@@ -125,9 +126,85 @@ function aplicarFiltros() {
   }
 
   pintarResultados(filtrados);
+
   //funcion para los textos y el badge
   actualizarEstadisticas(filtrados);
 }
+
+//funcion para presentar las estadisticas ------------------------------
+function actualizarEstadisticas(lista){
+  //usamos el ejercicio 11
+  const vivos = contarVivos(lista);
+  //cantidad de muertos con filter
+  const muertos = lista.filter(item => item.status === "Dead").length;
+  //cantidad de desconocidos con filter
+  const desconocidos = lista.filter(item => item.status === "unknown").length;
+
+  //busco elemento con id= estaisticas
+  //para con el .textContent reemplazar su contenido con lo que quiera
+
+  document.querySelector("#estadisticas").textContent =
+    `${vivos} Vivos - ${muertos} Muertos - ${desconocidos} Desconocidos`;
+
+  //guardo en la variable aviso la referencia a aviso-muertos
+  const aviso = document.querySelector("#aviso-muertos");
+
+  //Condicion si hay muertos muestre algo, y si no algo distinto
+  //uso la funcion del ejercicio 6
+  if (hayPersonajesMuertos(lista)){
+    //si hay muertos cambia el texto del aviso y pone eso
+    aviso.textContent = "Hay muertos en el resultado";
+    aviso.className = "badge-aviso badge-muertos"
+    //cambio el estilo del css para que se vea
+    aviso.style.display = "inline-block"
+    
+    //si no hay muertos y en cambio todos estan vivos avisa y pone eso
+  }else if (lista.length > 0 && todosVivos(lista)) {
+    aviso.textContent = "Todos vivos";
+    aviso.className = "badge-aviso badge-vivos";
+    aviso.style.display = "inline-block";
+    
+  } else {
+    //No pongo nada porque no hay personajes muertos ni estan todos vivos
+    aviso.textContent = "";
+    aviso.className = "badge-aviso"
+    //oculto el mensaje de la vista
+    aviso.style.display = "none"
+  }
+    
+}
+
+
+//funcion para la funcionalidad al presionar el orden A-Z ----------------------------------
+//busco el botón en el html
+//escucha si hago clic en el y ejecuta la funcion cada vez que se da clic
+document.querySelector("#btn-ordenar").addEventListener("click", function(){
+  //si se presiona el estado de ordenadoAZ pasa de False a True
+  ordenadoAZ = !ordenadoAZ
+  //el this referencia la boton
+  //el toogle agrega o quita la clase css "activo" dependiendo de ordenadoAZ para resaltarlo o no visualmente
+  this.classList.toggle("activo", ordenadoAZ)
+  //fitrar todo y mostrar en pantalla el cambio
+  aplicarFiltros();
+})
+
+//reaccionar al checkbox
+//busca la casilla de verificacion con el id "#check-primeros"
+//escucha el evento "change" que se activa al marcar la casilla y ejecuta la funcion aplicarFiltros
+document.querySelector("#check-primeros").addEventListener("change", aplicarFiltros);
+
+//funcionalidad para el botón limpiar filtros ----------------------------------------------------
+document.querySelector("#btn-limpiar").addEventListener("click", function () {
+  //reinicia los valores que tengan los filtros
+  document.querySelector("#filtro-nombre").value = "";
+  document.querySelector("#filtro-estado").value = "";
+  document.querySelector("#filtro-especie").value = "";
+  document.querySelector("#check-primeros").checked = false;
+  ordenadoAZ = false;
+  document.querySelector("#btn-ordenar").classList.remove("activo");
+  aplicarFiltros();
+});
+
 
 function pintarResultados(lista) {
   const contenedor = document.querySelector("#resultados");
